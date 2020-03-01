159e5f6138ffb84012c342f1b9673149ba2badda
user <user@example.com>
2020-03-01 16:44:22 +0200

    Jobs and coolers

--- send.php
+++ send.php
@@ -6,9 +6,19 @@ if(!$_POST['email']){ //если пустой/отсутствует
  }
 
 if(!$_POST['phone']  || !$_POST['name']){ // антиспам
-    die('BOT, FUCK OFF FROM MY SITE!!!');
-}
+     exit("<center>
+              <table width='600' height='300' align='center'>
+           <tr>
+           <td class='warning_table' width='220' align='center' valign='middle'>
+           <div align='center' class='warning_font_big'>Спасибо!</div>
+           <div align='center' class='warning_font' align='left'>Ваш заказ передан в отдел доставки.<br />Для уточнения деталей в ближайшее время с Вами свяжется наш менеджер.</div>
+           <p align='center'><a href='index.html' class='all_links'>Вернуться назад</a></div></p>
+           </td>
+           </tr>
+           </table></center>");
+ }
 
+if ($_POST['emoil'] != '') exit('Спасибо! Ваш заказ передан в отдел доставки.');
 
 $sendto   = "sales@imperatorska.com.ua"; // почта, на которую будет приходить письмо
 $username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
@@ -16,8 +26,10 @@ $usertel = $_POST['phone']; // сохраняем в переменную данные полученные из поля 
 $usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
 // $userbottle = $_POST['tara'];
 $usemess = $_POST['message'];
-// $userpromo = $_POST['promo'];
- 
+$userpromo = $_POST['promo'];
+
+
+
 // Формирование заголовка письма
 $subject  = "New order for water delivery Imperatorska";
 $headers  = "From: " . strip_tags($usermail) . "\r\n";
@@ -32,7 +44,7 @@ $msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
 $msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
 // $msg .= "<p><strong>Залоговая тара:</strong> ".$userbottle."</p>\r\n";
 $msg .= "<p><strong>Детали заказа:</strong> ".$usemess."</p>\r\n";
-// $msg .= "<p><strong>ПРОМОКОД:</strong> ".$userpromo."</p>\r\n";
+$msg .= "<p><strong>ПРОМОКОД:</strong> ".$userpromo."</p>\r\n";
 $msg .= "</body></html>";
 
 // отправка сообщения
@@ -47,7 +59,28 @@ if(@mail($sendto, $subject, $msg, $headers)) {
  </td>
  </tr>
  </table></center>";
-} else {
+ 
+ <!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2487806454772455');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2487806454772455&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+ 
+ 
+ 
+ } else {
     echo "<center><table width='600' height='300' align='center'>
  <tr>
  <td class='warning_table' width='220' align='center' valign='middle'>
